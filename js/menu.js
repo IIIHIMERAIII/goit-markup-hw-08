daa0f86b29b84a0b06a3682d0d20cf8e7e2b6da3
Maksym <PTNPNH722@gmail.com>
2022-09-14 11:17:02 +0300

    final 2

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,6 @@
-(() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
 
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-    mobileMenuRef.classList.toggle("is-open");
-  });
-})();
+document.querySelector('.menu-btn').addEventListener('click',()=> {
+document.querySelector('.mobile-menu').classList.toggle('active');
+document.querySelector('.menu-btn').classList.toggle('active');
+document.body.classList.toggle('overflow');
+});
